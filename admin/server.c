110e82934e9ac915ed6acf7471d7c72fbec47f07
TomaAlin01 <alintoma03@gmail.com>
2024-06-12 22:14:49 +0300

    Update server.c

--- admin/server.c
+++ admin/server.c
@@ -11,17 +11,22 @@
 #include <netinet/in.h>
 
 #define PORT 12345
+#define ADMIN_PORT 12346
 #define BUFFER_SIZE 1024
 #define LOCALE_DIR "locale"
 #define DOMAIN "messages"
 
 void *handle_client(void *client_socket);
-
 void translate_text(const char *input, char *output);
+void *handle_admin_request(void *admin_socket);
+
+int connected_clients = 0;
+pthread_mutex_t client_counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main() {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
+    int server_socket, admin_socket;
+    int client_socket, admin_client_socket;
+    struct sockaddr_in server_addr, client_addr, admin_addr;
     socklen_t addr_size;
     pthread_t tid;
 
@@ -32,9 +37,20 @@ int main() {
     }
     printf("Server socket created successfully\n");
 
+    admin_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (admin_socket < 0) {
+        perror("Admin socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Admin socket created successfully\n");
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    admin_addr.sin_family = AF_INET;
+    admin_addr.sin_port = htons(ADMIN_PORT);
+    admin_addr.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Socket binding failed");
@@ -42,28 +58,70 @@ int main() {
     }
     printf("Socket binding successful\n");
 
+    if (bind(admin_socket, (struct sockaddr *)&admin_addr, sizeof(admin_addr)) < 0) {
+        perror("Admin socket binding failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Admin socket binding successful\n");
+
     if (listen(server_socket, 10) < 0) {
         perror("Listening failed");
         exit(EXIT_FAILURE);
     }
     printf("Server listening on port %d\n", PORT);
 
+    if (listen(admin_socket, 10) < 0) {
+        perror("Admin listening failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Admin listening on port %d\n", ADMIN_PORT);
+
     while (1) {
-        addr_size = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
-        if (client_socket < 0) {
-            perror("Connection accept failed");
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(server_socket, &readfds);
+        FD_SET(admin_socket, &readfds);
+        int max_sd = server_socket > admin_socket ? server_socket : admin_socket;
+
+        int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if (activity < 0) {
+            perror("Select error");
             exit(EXIT_FAILURE);
         }
-        printf("Client connected\n");
 
-        if (pthread_create(&tid, NULL, handle_client, (void *)&client_socket) != 0) {
-            perror("Thread creation failed");
-            exit(EXIT_FAILURE);
+        if (FD_ISSET(server_socket, &readfds)) {
+            addr_size = sizeof(client_addr);
+            client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size);
+            if (client_socket < 0) {
+                perror("Connection accept failed");
+                exit(EXIT_FAILURE);
+            }
+            printf("Client connected\n");
+
+            pthread_mutex_lock(&client_counter_mutex);
+            connected_clients++;
+            pthread_mutex_unlock(&client_counter_mutex);
+
+            if (pthread_create(&tid, NULL, handle_client, (void *)&client_socket) != 0) {
+                perror("Thread creation failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if (FD_ISSET(admin_socket, &readfds)) {
+            addr_size = sizeof(admin_addr);
+            admin_client_socket = accept(admin_socket, (struct sockaddr *)&admin_addr, &addr_size);
+            if (admin_client_socket >= 0) {
+                if (pthread_create(&tid, NULL, handle_admin_request, (void *)&admin_client_socket) != 0) {
+                    perror("Admin thread creation failed");
+                    exit(EXIT_FAILURE);
+                }
+            }
         }
     }
 
     close(server_socket);
+    close(admin_socket);
     return 0;
 }
 
@@ -79,6 +137,9 @@ void *handle_client(void *client_socket) {
     if (file == NULL) {
         perror("File open failed");
         close(sock);
+        pthread_mutex_lock(&client_counter_mutex);
+        connected_clients--;
+        pthread_mutex_unlock(&client_counter_mutex);
         pthread_exit(NULL);
     }
 
@@ -91,6 +152,9 @@ void *handle_client(void *client_socket) {
     if (file == NULL) {
         perror("File open failed");
         close(sock);
+        pthread_mutex_lock(&client_counter_mutex);
+        connected_clients--;
+        pthread_mutex_unlock(&client_counter_mutex);
         pthread_exit(NULL);
     }
     fwrite(translated_text, sizeof(char), strlen(translated_text), file);
@@ -99,6 +163,9 @@ void *handle_client(void *client_socket) {
     write(sock, "translated.txt", strlen("translated.txt") + 1);
 
     close(sock);
+    pthread_mutex_lock(&client_counter_mutex);
+    connected_clients--;
+    pthread_mutex_unlock(&client_counter_mutex);
     pthread_exit(NULL);
 }
 
@@ -110,3 +177,20 @@ void translate_text(const char *input, char *output) {
     strcpy(output, gettext(input));
 }
 
+void *handle_admin_request(void *admin_socket) {
+    int sock = *(int *)admin_socket;
+    char buffer[BUFFER_SIZE];
+
+    recv(sock, buffer, BUFFER_SIZE, 0);
+
+    if (strcmp(buffer, "GET_CLIENT_COUNT") == 0) {
+        char client_count[10];
+        pthread_mutex_lock(&client_counter_mutex);
+        snprintf(client_count, 10, "%d", connected_clients);
+        pthread_mutex_unlock(&client_counter_mutex);
+        send(sock, client_count, strlen(client_count) + 1, 0);
+    }
+
+    close(sock);
+    pthread_exit(NULL);
+}
