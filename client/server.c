e25fd83ac81226cb731efe32ec47a8c37f98673c
dariusalba <paul.darius34@yahoo.com>
2024-06-12 23:39:25 +0300

    abandoned gsoap

--- client/server.c
+++ client/server.c
@@ -1,16 +1,9 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <errno.h>
-#include <sys/wait.h>
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <resolv.h> 
-#include <time.h> 
+#include <microhttpd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+<<<<<<< Updated upstream
 #define SERV_TCP_PORT 8080
 #define MAXLINE 512
 #define MAX_CLIENTS 50
@@ -67,5 +60,50 @@ int main(int argc, char *argv[])
             close(newsockfd);
             wait(NULL);
         }
+=======
+#define PORT 8888
+
+int request_handler(void *cls, struct MHD_Connection *connection, const char *url, 
+                   const char *method, const char *version, const char *upload_data,
+                   size_t *upload_data_size, void **con_cls) {
+    if (strcmp(method, "POST") != 0) {
+        return MHD_NO;
+>>>>>>> Stashed changes
     }
+
+    static int dummy;
+    if (&dummy != *con_cls) {
+        *con_cls = &dummy;
+        return MHD_YES;
+    }
+
+    if (*upload_data_size != 0) {
+        // Here we should save the upload data to a file or a buffer
+        // This example just prints the uploaded data
+        printf("Received data: %s\n", upload_data);
+        *upload_data_size = 0;
+        return MHD_YES;
+    } else {
+        const char *translated_text = "Translated text";
+
+        struct MHD_Response *response = MHD_create_response_from_buffer(strlen(translated_text), 
+                                                                        (void *)translated_text, MHD_RESPMEM_PERSISTENT);
+        int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+        MHD_destroy_response(response);
+        return ret;
+    }
+}
+
+int main() {
+    struct MHD_Daemon *daemon;
+
+    daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, 
+                              &request_handler, NULL, MHD_OPTION_END);
+    if (NULL == daemon) return 1;
+
+    printf("Server running on port %d\n", PORT);
+    getchar();
+
+    MHD_stop_daemon(daemon);
+    return 0;
 }
